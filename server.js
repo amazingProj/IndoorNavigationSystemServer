e05f825bfb03de73e552fe36d3e797d754b75fef
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2022-03-24 11:04:36 +0200

    first commit2

--- server.js
+++ server.js
@@ -1,35 +1,61 @@
+require('dotenv').config();
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
-
 const app = express();
 app.use(index);
-
 const server = http.createServer(app);
-
 const io = socketIo(server);
+var mqtt = require('mqtt')
+var url = 'wss://712d6a94edd544ddac8b5c44600f18d3.s1.eu.hivemq.cloud:8884/mqtt'
+var options = {
+    username: 'Esp32',
+    password: 'Esp32Asaf',
+    wsOptions : true
+}
+var subscribeTopic = "users/devices/location"
+//initialize the MQTT client
+var mqttClient = mqtt.connect(url,options);
+mqttClient.subscribe(subscribeTopic);
 
-let interval;
+mqttClient.on('connect', function () {
+  console.log('Connected');
+ 
+});
+
+mqttClient.on('message', function (topic, message) 
+{
+    let infoMessage = JSON.parse(message);
+    let messageString = message.toString()
+    console.log('Received message:', topic, messageString);
+    if (topic == "users/devices/location"){
+      let userID = infoMessage["ID"];
+      console.log(userID);
+      io.sockets.emit(userID, infoMessage)
+
+    }
+});
+
+mqttClient.on('error', function (error) {
+  console.log(error);
+  client = mqtt.connect(options)
+});
 
 io.on("connection", (socket) => {
   console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
+
+  socket.on("data", () =>
+  {
+
+  });
+
+
+  socket.on("disconnect", () => 
+  {
     console.log("Client disconnected");
-    clearInterval(interval);
   });
 });
-
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
